2b69be71e8eaaf1cd9ede0d6bd60a1fcee36ac42
joegilberto <joekgilberto@gmail.com>
2023-09-03 16:45:37 -0400

    fixes auction repeats

--- controllers/user.js
+++ controllers/user.js
@@ -11,25 +11,37 @@ async function index(req, res, next) {
     try {
         const id = req.params.id
         const currentUser = await User.findById(id)
-        const listings = await Listing.find({user: currentUser._id});
+        const listings = await Listing.find({ user: currentUser._id });
         listings.sort((a, b) => {
             return b.listingDate - a.listingDate
         })
 
         const auctions = []
-        const postedAuctions = await Auction.find({user: id})
-        postedAuctions.forEach(async (postedAuction)=>{
-            auctions.push(await Listing.findById(postedAuction.listing))
-        })
+        const postedAuctions = await Auction.find({ user: id })
+        for (let postedAuction of postedAuctions) {
+            let alreadyIn = false
+            let foundListing = await Listing.findById(postedAuction.listing)
+
+            for (auction of auctions){
+                if (postedAuction.listing.equals(auction._id)){
+                    alreadyIn = true
+                }
+            }
+
+            if (!alreadyIn){
+                auctions.push(foundListing)
+            }
+            
+        }
 
         auctions.sort((a, b) => {
             return b.listingDate - a.listingDate
         })
-        
-        const allCategories = await Category.find().sort('title');
-        console.log("auctions",auctions)
 
-        res.render('user/index', { title: currentUser.name, listings, categories: allCategories, auctions});
+        const allCategories = await Category.find().sort('title');
+
+        res.render('user/index', { title: currentUser.name, listings, categories: allCategories, auctions });
+
     } catch (err) {
         console.log(err);
         next(err);
